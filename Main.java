88cb6996945488a8449b5dfec526c426a4f8321a
Elena55555 <kastalskayaelena@gmail.ru>
2024-12-01 22:39:22 +0300

    1 HW Intellij idea development environment

--- Main.java
+++ Main.java
@@ -1,3 +1,8 @@
 public class Main {
-
+    public static void main(String[] args) {
+        /**output the following line
+         * to the console
+         */
+        System.out.println("Привет, Elena");
+    }
 }
